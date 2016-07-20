d4e7b3a8da7c2908fab514981bee42522ac3d0f4
lilJay-lin <8293526jie@>
2016-07-21 00:42:35 +0800

    添加cache-control跨域支持

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 //设置跨域访问
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,cache-control");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
